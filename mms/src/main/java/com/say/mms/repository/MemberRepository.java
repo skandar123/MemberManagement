75fbbd77ce7730ce245ad86a9ef0037d44db4e5d
Sayantika Kandar <sayantika@truetest.in>
2025-07-05 16:44:23 +0530

    deleted old files

--- mms/src/main/java/com/say/mms/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.say.mms.repository;
-
-import com.say.mms.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
